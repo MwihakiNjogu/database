9a0ebb26db248088bf91c2622dc98c206fc975d2
unknown <mwihakinjogu79@gmail.com>
2023-02-28 16:20:50 +0300

    first commit

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from peewee import *
-from os import_path
+from os import path
 connection = path.dirname(path.realpath(__file__))
 db = SqliteDatabase(path.join("mwihaki.db"))
 
@@ -7,6 +7,7 @@ db = SqliteDatabase(path.join("mwihaki.db"))
 
 class Student(Model):
     stud_name = CharField()
+    stud_phone = CharField()
     stud_email = CharField()
     stud_password = Charfield()
 
@@ -17,3 +18,26 @@ Student.create_table(fail_silently=True)
 
 
 
+class Teacher(Model):
+    teach_name = CharField()
+    teach_email = CharField()
+    teach_password = Charfield()
+Teacher.create_table(fail_silently=True)
+
+
+class Product(Model):
+    prod_price = CharField()
+    prod_quantity = CharField()
+    prod_description = Charfield()
+    prod_color = Charfield()
+
+
+Product.create_table(fail_silently=True)
+
+class Users(Model):
+    usr_name = CharField()
+    usr_phone = CharField()
+    usr_email = Charfield()
+    usr_password = Charfield()
+
+User.create_table(fail_silently=True)
